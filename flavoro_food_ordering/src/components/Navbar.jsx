cbd9e854ffd2364e2cb5a0f437289072b46e07f6
Arpana bharti <arpanabharti09@gmail.com>
2025-07-22 07:05:51 +0530

    create project

---
+++ flavoro_food_ordering/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-col lg:flex-row justify-between pt-2.5 pr-4'>
+        <div className='pl-5'>
+            <h3 className='font-semibold text-xl text-gray-600'>{(new Date()).toUTCString().slice(0,16)}</h3>
+            <h1 className='text-2xl font-bold'>Flavoro Foods</h1>
+        </div>
+       <div>
+        <input type="search" placeholder='Search' className='border p-2.5 w-full lg:w-[25vw] rounded-lg border-gray-400 outline-none'/>
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
